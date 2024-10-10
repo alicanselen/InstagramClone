baede672140eb7831c5ca6b246ebf5181ff63d4b
alicanselen <81762458+alicanselen@users.noreply.github.com>
2024-10-10 15:25:02 +0300

    Image Picker Eklendi

--- app/(tabs)/new.tsx
+++ app/(tabs)/new.tsx
@@ -1,21 +1,43 @@
 import { Stack } from 'expo-router';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View  , Image, TextInput, Pressable} from 'react-native';
-
+import * as ImagePicker from 'expo-image-picker';
 import { ScreenContent } from '~/components/ScreenContent';
 
 export default function Home() {
   const [caption , setCaption]= useState('');
+  const [image , setImage] = useState<string | null>(null);
+
+  useEffect(()=>{
+    if(!image){
+      pickImage();
+    }
+  } , [image]);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
   return (
     <View className='p-3 items-center flex-1'>
       {/* imagge Picker*/}
-      <Image
+      {image ? (<Image
       source={{
-        uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg'
+        uri: image
       }}
       className='w-52 aspect-[3/4] rounded-lg bg-slate-300'/>
+    ):(<View className='w-52 aspect-[3/4] rounded-lg bg-slate-300'/>)}
 
-      <Text onPress={()=>{}} className='text-blue-500 font-semibold m-5'>Change</Text>
+      <Text onPress={pickImage} className='text-blue-500 font-semibold m-5'>Change</Text>
 
       {/* Caption Area*/}
 
